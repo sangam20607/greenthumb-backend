daa6c8f6b6f4a568ef609b74a4447281f9f01ab7
Sangam Sharma <sangam20607@gmail.com>
2026-04-19 02:50:13 +0530

    fix token Bearer auth issue

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -8,11 +8,12 @@ const authMiddleware = (req, res, next) => {
   }
 
   try {
+    const token = authHeader.split(" ")[1];
     const decoded = jwt.verify(token, "secretkey");
     req.user = decoded;
     next();
   } catch (err) {
-    res.status(401).json({ message: "Invalid token" });
+    return res.status(401).json({ message: "Invalid token" });
   }
 };
 
