77abba6aa6bfedc0886f2f40e788464a73d392da
Sangam Sharma <sangam20607@gmail.com>
2026-04-18 23:08:24 +0530

    added middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req, res, next) => {
+  try {
+    const token = req.headers["authorization"];
+
+    if (!token) {
+      return res.status(401).json({ message: "No token, access denied" });
+    }
+
+    const decoded = jwt.verify(token, "secretkey");
+
+    req.user = decoded;
+
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+module.exports = authMiddleware;
