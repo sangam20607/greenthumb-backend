2e3028b13ae5c14bade61235e05387b148d2a115
Sangam Sharma <sangam20607@gmail.com>
2026-04-19 03:01:43 +0530

    fixed auth middleware, signup, and plant filter issues

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,19 +1,20 @@
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
-  const token = req.headers.authorization;
+  const authHeader = req.headers.authorization;
 
-  if (!token) {
+  if (!authHeader) {
     return res.status(401).json({ message: "No token" });
   }
 
+  const token = authHeader.split(" ")[1]; // 🔥 FIX
+
   try {
-    const token = authHeader.split(" ")[1];
     const decoded = jwt.verify(token, "secretkey");
     req.user = decoded;
     next();
   } catch (err) {
-    return res.status(401).json({ message: "Invalid token" });
+    res.status(401).json({ message: "Invalid token" });
   }
 };
 
